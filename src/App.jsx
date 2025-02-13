70effac04a27bc7e7ac4f329c22d18a5996b6c44
Sebastian Klotz <basti.klotz@bluewin.ch>
2025-02-13 20:00:00 +0100

    move to own component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import { scaleTime, timeMonth, timeYear } from "d3"
+import { max, min, scaleTime, timeMonth, timeYear } from "d3"
+import { addMonths, format, monthsToQuarters, subMonths } from "date-fns"
 import "./App.css"
-import { format, monthsToQuarters } from "date-fns"
 
 const rows = [
   {
@@ -11,7 +11,7 @@ const rows = [
         label: "QG1",
         isMilestone: true,
         start: new Date(2024, 1, 15),
-        finish: new Date(2024, 1, 15),
+        finish: new Date(2023, 10, 15),
         // baselineStart:
         //   baselineFinish:
         // actualStart:
@@ -35,7 +35,7 @@ const rows = [
         label: "QG3",
         isMilestone: true,
         start: new Date(2024, 1, 15),
-        finish: new Date(2024, 6, 7),
+        finish: new Date(2025, 2, 13),
         // baselineStart:
         //   baselineFinish:
         // actualStart:
@@ -46,8 +46,8 @@ const rows = [
         id: 2,
         label: "Task 1",
         isMilestone: false,
-        start: new Date(2024, 1, 15),
-        finish: new Date(2024, 6, 7),
+        start: new Date(2023, 10, 15),
+        finish: new Date(2025, 2, 13),
         progress: 0.8,
       },
     ],
@@ -765,7 +765,29 @@ const Activity = ({ xScale, rowPadding, height, padding, data }) => {
   )
 }
 
-function App() {
+const Axis = ({ ticks, range, tickLabelPadding, xOffset }) => {
+  return (
+    <g transform={`translate(0, ${xOffset})`}>
+      <path d={["M", range[0], 0, "L", range[1], 0].join(" ")} fill="none" stroke="currentColor" />
+      {ticks.map(({ value, xOffset }) => (
+        <g key={value} transform={`translate(${xOffset}, 0)`}>
+          <line y2={-6} stroke="currentColor" />
+          <text
+            transform={`translate(0, ${-tickLabelPadding})`}
+            style={{
+              fontSize: "10px",
+              textAnchor: "middle",
+            }}
+          >
+            {format(value, "yyyy")}
+          </text>
+        </g>
+      ))}
+    </g>
+  )
+}
+
+const GanttChart = () => {
   const w = 1200
   const labelWidth = 200
   const tickLabelPadding = 14
@@ -776,22 +798,22 @@ function App() {
   const activityPadding = 5
   const milestoneSize = activityHeight + 4
   const h = rows.length * rowHeight + 3 * 40 + 20
-  const minDate = new Date(2024, 0, 1)
-  const maxDate = new Date(2025, 3, 30)
+  const minDate = min(rows.reduce((acc, val) => [...acc, ...val.activities], []).map((el) => el.finish))
+  const maxDate = max(rows.reduce((acc, val) => [...acc, ...val.activities], []).map((el) => el.finish))
   const dataDate = new Date(2024, 5, 21)
-  const x = scaleTime([minDate, maxDate], [0, w])
-  const range = x.range()
+  const scale = scaleTime([subMonths(minDate, 1), addMonths(maxDate, 1)], [0, w])
+  const range = scale.range()
 
-  const yearScale = scaleTime([minDate, maxDate], [0, w])
-  const yearTicks = yearScale.ticks(timeYear).map((el) => ({ value: el, xOffset: yearScale(el) }))
-  const quarterScale = scaleTime([minDate, maxDate], [0, w])
-  const quarterTicks = quarterScale.ticks(timeMonth.every(3)).map((el) => ({ value: el, xOffset: quarterScale(el) }))
-  const monthScale = scaleTime([minDate, maxDate], [0, w])
-  const monthTicks = monthScale.ticks(timeMonth).map((el) => ({ value: el, xOffset: monthScale(el) }))
+  const formatYearTick = (dt) => format(dt, "yyyy")
+  const formatQuarterTick = (dt) => format(dt, "yyyy")
+  const formatMonthTick = (dt) => format(dt, "yyyy")
+
+  const yearTicks = scale.ticks(timeYear).map((el) => ({ value: el, xOffset: scale(el) }))
+  const quarterTicks = scale.ticks(timeMonth.every(3)).map((el) => ({ value: el, xOffset: scale(el) }))
+  const monthTicks = scale.ticks(timeMonth).map((el) => ({ value: el, xOffset: scale(el) }))
 
   return (
     <div>
-      <p>Gantt</p>
       <svg width={w} height={h}>
         <g className="grid-lines grid-lines-vertical" transform={`translate(${labelWidth}, 120)`}>
           {monthTicks.map(({ value, xOffset }) => (
@@ -805,60 +827,9 @@ function App() {
           ))}
         </g>
         <g transform={`translate(${labelWidth}, 0)`}>
-          <g transform="translate(0, 40)">
-            <path d={["M", range[0], 0, "L", range[1], 0].join(" ")} fill="none" stroke="currentColor" />
-            {yearTicks.map(({ value, xOffset }) => (
-              <g key={value} transform={`translate(${xOffset}, 0)`}>
-                <line y2={-6} stroke="currentColor" />
-                <text
-                  key={value}
-                  transform={`translate(0, ${-tickLabelPadding})`}
-                  style={{
-                    fontSize: "10px",
-                    textAnchor: "middle",
-                  }}
-                >
-                  {format(value, "yyyy")}
-                </text>
-              </g>
-            ))}
-          </g>
-          <g transform="translate(0, 80)">
-            <path d={["M", range[0], 0, "L", range[1], 0].join(" ")} fill="none" stroke="currentColor" />
-            {quarterTicks.map(({ value, xOffset }) => (
-              <g key={value} transform={`translate(${xOffset}, 0)`}>
-                <line y2={-6} stroke="currentColor" />
-                <text
-                  key={value}
-                  transform={`translate(0, ${-tickLabelPadding})`}
-                  style={{
-                    fontSize: "10px",
-                    textAnchor: "middle",
-                  }}
-                >
-                  {`Q${monthsToQuarters(value.getMonth()) + 1}`}
-                </text>
-              </g>
-            ))}
-          </g>
-          <g transform="translate(0, 120)">
-            <path d={["M", range[0], 0, "L", range[1], 0].join(" ")} fill="none" stroke="currentColor" />
-            {monthTicks.map(({ value, xOffset }) => (
-              <g key={value} transform={`translate(${xOffset}, 0)`}>
-                <line y2={-6} stroke="currentColor" />
-                <text
-                  key={value}
-                  transform={`translate(0, ${-tickLabelPadding})`}
-                  style={{
-                    fontSize: "10px",
-                    textAnchor: "middle",
-                  }}
-                >
-                  {format(value, "MMM")}
-                </text>
-              </g>
-            ))}
-          </g>
+          <Axis ticks={yearTicks} range={range} tickLabelPadding={tickLabelPadding} xOffset={40} />
+          <Axis ticks={quarterTicks} range={range} tickLabelPadding={tickLabelPadding} xOffset={80} />
+          <Axis ticks={monthTicks} range={range} tickLabelPadding={tickLabelPadding} xOffset={120} />
         </g>
         <g transform={`translate(${labelWidth}, 120)`}>
           {rows.map((row, idx) => (
@@ -871,7 +842,7 @@ function App() {
                     rowPadding={rowPadding}
                     height={activityHeight}
                     padding={activityPadding}
-                    xScale={monthScale}
+                    xScale={scale}
                     data={a}
                   />
                 ))}
@@ -883,7 +854,7 @@ function App() {
                     rowPadding={rowPadding}
                     activityHeight={activityHeight}
                     size={milestoneSize}
-                    xScale={monthScale}
+                    xScale={scale}
                     data={a}
                   />
                 ))}
@@ -902,9 +873,17 @@ function App() {
           ))}
         </g>
         <g className="data-date" transform="translate(40, 20)">
-          <line y2={h} strokeWidth="2" transform={`translate(${monthScale(dataDate)}, 0)`} />
+          <line y2={h} strokeWidth="2" transform={`translate(${scale(dataDate)}, 0)`} />
         </g>
       </svg>
+    </div>
+  )
+}
+
+function App() {
+  return (
+    <div>
+      <GanttChart />
     </div>
   )
 }
